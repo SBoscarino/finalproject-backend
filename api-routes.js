fe82a6cf5a4061a3992603e1417d770c8c39e124
Stefanie Boscarino <stefanie.boscarino@gmail.com>
2017-10-02 13:46:43 -0500

    changed deletion route

--- api-routes.js
+++ api-routes.js
@@ -6,16 +6,22 @@ const router = express.Router();
 //get all
 router.get('/api/todos', function(req, res) {
   Todo.find({})
-    .exec(function(err, sets) {
+    .exec(function(err, todos) {
       res.send(todos);
     });
 });
 
-// route for deleting one
+//delete one
+router.delete('/api/todos/delete/:id', (req, res) => {
+  Todo.deleteOne({_id: req.params.id}).then(function(){
+    console.log("in deletion route");
+    res.redirect('/');
+  }).catch(function(err){
+    res.redirect('/')
+    })
+});
 
-// route for updating one
-
-// route for adding one
+// add one
 router.post('/api/todos', (req, res) => {
   var newTodo = new Todo();
   newTodo.description = req.body.description;
