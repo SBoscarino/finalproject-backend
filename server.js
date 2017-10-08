ba63dda92476fe6f3cc431b9b3950ac4e7e9ba79
Stefanie Boscarino <stefanie.boscarino@gmail.com>
2017-10-07 20:33:02 -0500

    changes

--- server.js
+++ server.js
@@ -7,6 +7,52 @@ const alexa = require('alexa-app');
 
 const app = express();
 
+// Alexa things
+
+// Creates route POST /cohort
+const alexaApp = new alexa.app('cohort');
+
+alexaApp.express({
+  expressApp: app
+});
+
+alexaApp.launch(function(req, res) {
+  const prompt = 'Would you like to know more?';
+
+  res.say(prompt)
+    .reprompt(prompt)
+    .shouldEndSession(false);
+});
+
+alexaApp.intent('list', {
+  utterances: [
+    "list my todos",
+    "list my tasks"
+  ],
+}, function(req, res) {
+  res.say("You have nothing todo!");
+});
+
+alexaApp.intent('addTask', {
+  utterances: [
+    "add task",
+    "add todo"
+  ]
+}, function(req, res) {
+  res.say("Todo added!");
+});
+
+alexaApp.intent('deleteTask', {
+  utterances: [
+    "delete task",
+    "delete todo"
+  ]
+}, function(req, res) {
+  res.say("Todo deleted!");
+});
+
+// Rest of app
+
 app.use(express.static('public'));
 
 mongoose.connect(process.env.MONGODB_URI)
@@ -24,42 +70,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use(require('./api-routes.js'));
-
-// Alexa things
-
-// Creates route POST /cohort
-const alexaApp = new alexa.app('cohort');
-
-alexaApp.express({
-  expressApp: app
-});
-
-alexaApp.intent('list', {
-  utterances: [
-    "list my todos",
-    "list my tasks"
-  ],
-}, function(req, resp) {
-  resp.say("You have nothing todo!");
-});
-
-alexaApp.intent('addTask', {
-  utterances: [
-    "add task",
-    "add todo"
-  ]
-}, function(req, resp) {
-  resp.say("Todo added!");
-});
-
-alexaApp.intent('deleteTask', {
-  utterances: [
-    "delete task",
-    "delete todo"
-  ]
-}, function(req, resp) {
-  resp.say("Todo deleted!");
-});
 
 var port = process.env.PORT || 5003;
 
