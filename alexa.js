d8a0793adfce2ba1cb452fe9b73a36a9f8680a97
Stefanie Boscarino <stefanie.boscarino@gmail.com>
2017-10-09 20:31:56 -0500

    hey

--- alexa.js
+++ alexa.js
@@ -15,7 +15,7 @@ alexaApp.launch(function(req, res) {
       let reprompt = '';
 
       if (todos.length) {
-        say += ` You have ${todos.length} to-dos to complete. Say, description, to hear more about each.`;
+        say += ` You have ${todos.length} to-dos to complete. Say, description, to hear more about each, or add to-do to create a new task.`;
         reprompt = 'Would you like for me to list your tasks?';
       } else {
         say += ' You have no to-dos to do!';
